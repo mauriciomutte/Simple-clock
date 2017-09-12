f7aba5c997208e1686cfd4f57f310e5c3e0e4d54
Maurício Mutte <hi@mauriciomutte.com>
2017-09-12 18:21:50 -0300

    Add data script

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 const divClock = document.querySelector('.clock')
-divClock.addEventListener('onload', clock())
+const divData = document.querySelector('.data')
 
+divData.addEventListener('onload', data())
+function data(){
+  const date = new Date()
+  let week = date.getDay()
+  let day = date.getDate()
+  let month = date.getMonth()
+  let year = date.getFullYear()
+  // Week name
+  if (week === 0) {
+    week = 'Sun'
+  } else if (week === 1){
+    week = 'Mon'
+  } else if (week === 2){
+    week = 'Tues'
+  } else if (week === 3){
+    week = 'Wed'
+  } else if (week === 4){
+    week = 'Thurs'
+  } else if (week === 5){
+    week = 'Fri'
+  } else if (week === 6){
+    week = 'Sat'
+  }
+  // Month name
+  if (month === 0){
+    month = 'January'
+  } else if (month === 1){
+    month = 'February'
+  } else if (month === 2){
+    month = 'March'
+  } else if (month === 3){
+    month = 'April'
+  } else if (month === 4){
+    month = 'May'
+  } else if (month === 5){
+    month = 'June'
+  } else if (month === 6){
+    month = 'July'
+  } else if (month === 7){
+    month = 'August'
+  } else if (month === 8){
+    month = 'September'
+  } else if (month === 9){
+    month = 'October'
+  } else if (month === 10){
+    month = 'November'
+  } else if (month === 11){
+    month = 'December'
+  }
+
+  divData.innerHTML = `${week}, ${day} ${month} ${year}`
+}
+
+
+divClock.addEventListener('onload', clock())
 function clock(){
   const date = new Date()
   let hours = date.getHours()
