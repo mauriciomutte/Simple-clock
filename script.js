5a37a8fd1fb3ad8289c3711b297d54ba2207efb5
Maurício Mutte <hi@mauriciomutte.com>
2017-09-14 12:40:35 -0300

    Reduce date and clock script

--- script.js
+++ script.js
@@ -9,47 +9,11 @@ function data(){
   let month = date.getMonth()
   let year = date.getFullYear()
   // Week name
-  if (week === 0) {
-    week = 'Sun'
-  } else if (week === 1){
-    week = 'Mon'
-  } else if (week === 2){
-    week = 'Tues'
-  } else if (week === 3){
-    week = 'Wed'
-  } else if (week === 4){
-    week = 'Thurs'
-  } else if (week === 5){
-    week = 'Fri'
-  } else if (week === 6){
-    week = 'Sat'
-  }
+  const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+  week = weekdays[week]
   // Month name
-  if (month === 0){
-    month = 'January'
-  } else if (month === 1){
-    month = 'February'
-  } else if (month === 2){
-    month = 'March'
-  } else if (month === 3){
-    month = 'April'
-  } else if (month === 4){
-    month = 'May'
-  } else if (month === 5){
-    month = 'June'
-  } else if (month === 6){
-    month = 'July'
-  } else if (month === 7){
-    month = 'August'
-  } else if (month === 8){
-    month = 'September'
-  } else if (month === 9){
-    month = 'October'
-  } else if (month === 10){
-    month = 'November'
-  } else if (month === 11){
-    month = 'December'
-  }
+  const monthsOfTheYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  month = monthsOfTheYear[month]
 
   divData.innerHTML = `${week}, ${day} ${month} ${year}`
 }
@@ -62,17 +26,15 @@ function clock(){
   let minutes = date.getMinutes()
   let seconds = date.getSeconds()
 
-  if (hours < 10) {
-    hours = `0${hours}`
-  }
-  if (minutes < 10) {
-    minutes = `0${minutes}`    
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`
+  function checkZero(element){
+    if (!(element < 10)) {
+      return `${element}`
+    } else {
+      return element = `0${element}`
+    }
   }
 
-  divClock.innerHTML = `${hours}:${minutes}:${seconds}`
+  divClock.innerHTML = `${checkZero(hours)}:${checkZero(minutes)}:${checkZero(seconds)}`
 }
 
 setInterval('clock()', 1000)
